f32575365a02db60a997b024b0e1eda1cdecee05
Frame_Bassman <Framebassman@gmail.com>
2016-08-16 23:01:44 +0300

    Added login / password textboxes

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: './app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+    title: string;
+    login: string;
+    password: string;
+
+    constructor(){
+        this.title = 'Angular 2do'
+    }
+}
